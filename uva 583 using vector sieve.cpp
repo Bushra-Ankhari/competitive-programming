31771521fd1247f59ca8e264fd2c1c50f06a5d0c
Bushra-Ankhari <40654094+Bushra-Ankhari@users.noreply.github.com>
2018-11-26 14:22:29 +0600

    Add files via upload

---
+++ uva 583 using vector sieve.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+#define MAX 50000//root of 10^9
+
+vector<int>primes;
+
+
+void sieve(){
+    bool isprime[MAX];
+    for(int i=0;i<MAX;++i) {isprime[i]=true;}
+    for(int i=3;i*i<=MAX;i+=2){
+        if(isprime[i]){
+            for(int j=i*i;j<=MAX;j+=i)
+            {
+                isprime[j]=false;
+            }
+        }
+    }
+  //  for(int i=0;i<100;i++){
+    //    cout<<i<<" "<<(isprime[i]?"prime":"not prime")<<endl;
+   // }
+primes.push_back(2);
+for(int i=3;i<MAX;i+=2){
+        if(isprime[i])
+    primes.push_back(i);
+}
+//for(int i=0;i<=100;i++){
+    //cout<<primes[i]<<endl;
+//}
+//int len=primes.size();
+//cout<<len<<endl;
+}
+int main()
+{
+    sieve();
+
+	long n;
+	while ( ~scanf("%ld",&n) && n ) {
+		printf("%ld =",n);
+		bool first = true;
+		if ( n < 0 ) {
+			printf(" -1");
+			first = false;
+			n *= -1;
+		}
+		for ( int i = 0 ; n > 1 && i <5133 ; ++ i )
+			while ( n%primes[i] == 0 ) {
+				if ( first ) {
+					printf(" %d",primes[i]);
+					first = false;
+				}else printf(" x %d",primes[i]);
+				n /= primes[i];
+			}
+		if ( n != 1 ) {
+			if ( first ) printf(" %d\n",n);
+			else printf(" x %d\n",n);
+		}else printf("\n");
+	}
+
+	return 0;
+}
